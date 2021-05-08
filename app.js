75d153defb4d622d516c0adba6cf51be56bd4274
DarioMeliaBasus <dario@meliabasus.com>
2021-05-08 17:32:07 +0200

    Current Day version

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded());
-app.use(express.static("public")); 
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  res.render('list', {day: currentDay()});
 });
 
 app.listen(3000, function(){
   console.log("Server started on port 3000")
 })
+
+
+function currentDay(){
+  var listDays = ["Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"] ;
+  var dayIndex = new Date().getDay();
+  var today = listDays[dayIndex];
+  return today;
+}
